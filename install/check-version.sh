f46b53f5f66eb14e9098252b45fad55f522aa474
Steven Fry <sw.fry@outlook.com>
2024-12-21 11:41:41 -0500

    first commit

---
+++ install/check-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f /etc/os-release ]; then
+    echo "$(tput setaf 1)Error: Unable to determine OS. /etc/os-release file not found."
+    echo "Installation stopped."
+    exit 1
+fi
+
+. /etc/os-release
+
+# Check if running on Fedora 41
+if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID != 41" | bc) != 1 ]; then
+    echo "$(tput setaf 1)Error: OS requirement not met"
+    echo "You are currently running: $ID $VERSION_ID"
+    echo "OS required: Fedora 41"
+    echo "Installation stopped."
+    exit 1
+fi
