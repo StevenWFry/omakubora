7bada6847e9a8ee9628f66c35b183c8e4dfdb7b4
StevenWFry <87485513+StevenWFry@users.noreply.github.com>
2025-01-11 20:36:08 -0500

    Update app-gum.sh

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,6 +1,6 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
-GUM_VERSION="0.14.3" # Use known good version
+GUM_VERSION="0.14.5" # Use known good version
 wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_aarch64.rpm"
 sudo dnf install -y ./gum.rpm
 rm gum.rpm
