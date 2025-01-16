45cc99db1d2c3ba37df331899ed2791c50bd0fab
Steven Fry <sw.fry@outlook.com>
2025-01-16 11:10:25 -0500

    updated to proper gum download

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -2,7 +2,7 @@
 cd /tmp
 #GUM_VERSION="0.14.5" # Use known good version
 #wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_aarch64.rpm"
-wget https://github.com/charmbracelet/gum/releases/download/v0.15.0/gum-0.15.0-1.aarch64.rpm
-sudo dnf install -y ./gum-0.15.0-1.aarch64.rpm
-rm gum.rpm
+wget https://github.com/charmbracelet/gum/releases/download/v0.15.0/gum-0.15.0-1.x86_64.rpm
+sudo dnf install -y ./gum-0.15.0-1.x86_64.rpm
+rm gum-0.15.0-1.x86_64.rpm
 cd -
